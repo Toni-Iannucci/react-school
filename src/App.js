62f1c7168077410359c2be6da01290c193c05e9e
Toni Iannucci <toni.iannucci@gmail.com>
2023-06-27 09:56:21 +0200

    [Views] Mise en place des différentes views et du css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import React, { Component } from 'react'
-function App() {
-  return (
-    <div className="App">
-       <h1> Ceci est un exercice React </h1>
-    </div>
-  );
-}
+import Capsules from './Components/capsules/Capsules';
+import Launches from './Components/launches/Launches';
 
+function App() {
+  const [view, setView] = useState('view1');
+  const toggleView = () => {
+    setView(prevView => (prevView === 'view1' ? 'view2' : 'view1'));
+  };
+  return (
+    <div className="btnDiv">
+    <button onClick={toggleView}>Toggle View</button>
+    {view === 'view1' ? (
+      <div>
+        <h1>View 1</h1>
+        <Launches />
+      </div>
+    ) : (
+      <div>
+        <h1>View 2</h1>
+        <Capsules />
+      </div>
+    )}
+  </div>
+);
+}
 export default App;
